46cf484aa92d016958b32bd7e0ed056bcf99605f
Pepiuox <pepiuox@gmail.com>
2020-05-13 23:40:33 -0500

    Update
    Add menu table for edit menu in your page

--- sql/page.sql
+++ sql/page.sql
@@ -1,45 +1,38 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.6-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             10.3.0.5866
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF NOT EXISTS `menu` (
+  `idMenu` int(11) NOT NULL AUTO_INCREMENT,
+  `sort` int(11) DEFAULT NULL,
+  `page_id` int(11) DEFAULT NULL,
+  `title_page` varchar(100) DEFAULT NULL,
+  `link_page` varchar(100) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT 0,
+  PRIMARY KEY (`idMenu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Volcando estructura para tabla plataformasrl.page
+DROP TABLE IF EXISTS `page`;
 CREATE TABLE IF NOT EXISTS `page` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `language` int(11) NOT NULL DEFAULT 1,
   `position` int(11) NOT NULL DEFAULT 0,
-  `title` varchar(50) CHARACTER SET utf8 DEFAULT 'Title',
-  `link` varchar(150) CHARACTER SET utf8 DEFAULT '#',
-  `url` varchar(150) CHARACTER SET utf8 DEFAULT '#',
-  `keyword` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `classification` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `description` varchar(160) CHARACTER SET utf8 DEFAULT NULL,
-  `image` varchar(150) CHARACTER SET utf8 DEFAULT '#',
+  `title` varchar(50) DEFAULT 'Title',
+  `link` varchar(150) DEFAULT '#',
+  `url` varchar(150) DEFAULT '#',
+  `keyword` varchar(150) DEFAULT NULL,
+  `classification` varchar(150) DEFAULT NULL,
+  `description` varchar(160) DEFAULT NULL,
+  `image` varchar(150) DEFAULT '#',
   `type` int(11) DEFAULT NULL,
   `menu` int(11) DEFAULT 1,
-  `template` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `base_template` varchar(150) CHARACTER SET utf8 DEFAULT NULL,
-  `content` longtext CHARACTER SET utf8 DEFAULT NULL,
-  `style` longtext CHARACTER SET utf8 DEFAULT NULL,
+  `template` varchar(150) DEFAULT NULL,
+  `base_template` varchar(150) DEFAULT NULL,
+  `content` longtext DEFAULT NULL,
+  `style` longtext DEFAULT NULL,
   `starpage` int(11) DEFAULT 0,
   `level` int(11) DEFAULT 1,
   `parent` int(11) DEFAULT 0,
   `sort` int(11) DEFAULT 0,
   `active` int(11) DEFAULT 1,
   `update` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- La exportación de datos fue deseleccionada.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
