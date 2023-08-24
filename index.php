e17c4a3adb2efd6cf2f6a79754b7fe5a65063c14
Pepiuox <pepiuox@gmail.com>
2023-08-24 12:33:47 -0500

    Updated

--- index.php
+++ index.php
@@ -11,24 +11,5 @@ ini_set('display_errors', 1);
 if (!isset($_SESSION)) {
     session_start();
 }
-
-$currentDate = new DateTime();
-$connfile = 'config/dbconnection.php';
-if (file_exists($connfile)) {
-    $page = $_SERVER['PHP_SELF'];
-
-    include 'config/dbconnection.php';
-    require 'classes/UserClass.php';
-    require 'classes/GetVisitor.php';
-
-    $login = new UserClass();
-    $timestamp = $currentDate->format('Y-m-d H:i:s');
-    $visitor = new GetVisitor($timestamp);
-
-    include 'start.php';
-} else {
-    $_SESSION['PathInstall'] = "http://{$_SERVER['HTTP_HOST']}/";
-    header('Location: installer/install.php');
-    exit();
-}
+include 'elements/view.php';
 ?>
