13536a0f5cf1a13148e5d38ebf91559c8003f00f
Pepiuox <pepiuox@gmail.com>
2024-04-13 23:21:49 -0500

    Update footer.php

--- elements/footer.php
+++ elements/footer.php
@@ -1,4 +1,3 @@
-
 <script>
     $(document).ready(function () {
         $("a").find('.active').each(function () {
@@ -17,6 +16,3 @@
         });
     });
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.9/beautify.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.9/beautify-css.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.9/beautify-html.min.js"></script>
