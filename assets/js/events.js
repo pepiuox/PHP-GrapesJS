9bb4332230ccadb4468f3f61ad50229c62bca513
Pepiuox <pepiuox@gmail.com>
2024-04-14 00:22:18 -0500

    Delete assets/js directory

--- assets/js/events.js
+++
@@ -1,5 +0,0 @@
-import DynamicContentEvents from './dynamicContent/dynamicContent.events';
-export default (editor => {
-  const dce = new DynamicContentEvents(editor);
-  dce.onComponentRemove();
-});
--- assets/js/scripts.js
+++
@@ -1,4 +0,0 @@
-$("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
